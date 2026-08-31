0567fbb01511275fdd01bce426da895cbdc6bd12
max-amb <max@max-amb.com>
2026-08-31 17:34:18 +0100

    Moved flot into its own file

--- recipes/pkgs/vulnerablecode/_aboutcode-pipeline.nix
+++ recipes/pkgs/vulnerablecode/_aboutcode-pipeline.nix
@@ -2,30 +2,10 @@
   buildPythonPackage,
   fetchPypi,
   lib,
+  callPackage,
 }:
 let
-  flot = buildPythonPackage (finalAttrs: {
-    pname = "flot";
-    version = "0.7.3";
-    format = "wheel";
-
-    src = fetchPypi {
-      inherit (finalAttrs) pname version;
-      format = "wheel";
-      dist = "py3";
-      python = "py3";
-      hash = "sha256-uGSxlKD+iJPux/5ixrBvTJiNoL35O0L+UityiynKNb8=";
-    };
-
-    meta = {
-      description = "Make it easier to create Python packages. Build multiple Python packages from one repo easily.";
-      homepage = "https://github.com/aboutcode-org/flot";
-      license = lib.licenses.AND [
-        lib.licenses.bsd2
-        lib.licenses.bsd3
-      ];
-    };
-  });
+  flot = callPackage ./_flot.nix { };
 in
 buildPythonPackage (finalAttrs: {
   pname = "aboutcode.pipeline";
